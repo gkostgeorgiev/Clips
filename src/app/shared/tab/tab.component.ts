04c7fe9363157b01bf15a5a683f0c8b380d12421
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2022-11-25 19:53:59 +0200

    Implemented toggling tabs
    Looped through the tabs, implemented functionality for active tab and prevented default browser behaviour to remove the '#' when rerouting.

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabComponent implements OnInit {
 
-  constructor() { }
+  @Input() tabTitle = ''
+  @Input() active = false;
 
+  constructor() { }
+  
   ngOnInit(): void {
   }
 
