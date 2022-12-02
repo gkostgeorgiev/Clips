0a27deeda0e799fd4363acaf8e229924bbc5eb3a
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2022-12-03 01:45:33 +0200

    Imported AngularFire
    Set up firebase, installed and imported AngularFire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBtl8gpsYSsamylo-pWUEbQN6krytszaKE",
+    authDomain: "clips-fda6f.firebaseapp.com",
+    projectId: "clips-fda6f",
+    storageBucket: "clips-fda6f.appspot.com",
+    messagingSenderId: "125741110571",
+    appId: "1:125741110571:web:c44ffdc4e920e31bc3a16e"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBtl8gpsYSsamylo-pWUEbQN6krytszaKE",
+    authDomain: "clips-fda6f.firebaseapp.com",
+    projectId: "clips-fda6f",
+    storageBucket: "clips-fda6f.appspot.com",
+    messagingSenderId: "125741110571",
+    appId: "1:125741110571:web:c44ffdc4e920e31bc3a16e"
+  },
 };
 
 /*
