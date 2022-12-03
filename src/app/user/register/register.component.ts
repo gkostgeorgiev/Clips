858137db97ae786edbb0c1270a977a54f5b624b4
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2022-12-03 02:44:45 +0200

    Refactored the user registration process to a service

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,9 +9,8 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class RegisterComponent {
   constructor(
-    private auth: AngularFireAuth,
-    private db: AngularFirestore,
-    ) { };
+    private auth: AuthService
+  ) { };
 
   inSubmission = false;
 
@@ -62,25 +60,15 @@ export class RegisterComponent {
     this.alertColor = 'blue';
     this.inSubmission = true;
 
-    const { email, password } = this.registerForm.value
-
     try {
-      const userCred = await this.auth.createUserWithEmailAndPassword(
-        email as string, password as string,
-      );
-      await this.db.collection('users').add({
-        name: this.name.value,
-        email: this.email.value,
-        age: this.age.value,
-        phoneNumber: this.phoneNumber.value,
-      });
+      await this.auth.createUser(this.registerForm.value)
     } catch (e) {
       console.error(e);
 
       this.alertMsg = 'An unexpected error occurred. Please try again later.';
       this.alertColor = 'red';
       this.inSubmission = false;
-      return 
+      return
     }
 
     this.alertMsg = 'Success! Your account has been created.';
