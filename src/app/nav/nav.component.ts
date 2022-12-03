31f417e71dd6d8d354502fdecc7ca2d3d96cb300
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2022-12-03 06:21:55 +0200

    Implemented functionality to sign out

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
 
 @Component({
   selector: 'app-nav',
@@ -12,6 +13,7 @@ export class NavComponent implements OnInit {
   constructor(
     public modal: ModalService,
     public auth: AuthService,
+    private afAuth: AngularFireAuth
   ) {
   }
 
@@ -22,5 +24,11 @@ export class NavComponent implements OnInit {
     $event.preventDefault();
 
     this.modal.toggleModal('auth');
+  }
+
+  async logout($event: Event){
+    $event.preventDefault();
+
+    await this.afAuth.signOut();
   }
 }
