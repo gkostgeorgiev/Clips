54318b39dab51ab7d8ffbc56dfdc01f2e5b2b724
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2022-12-06 21:38:01 +0200

    Updated upload functionality 2
    Used reactive forms to make the upload form functional. Registered the reactive form with the upload component.
    
    Connected the file name to the title field and implemented a method that removes file extension (.mp4) from the title.
    
    Installed uuid and used it to create unique ids for the corresponding videos that are being uploaded.
    
    Added an alert component to display the status of the upload.
    
    Created an upload progress observable and used it to show the percentage of the upload progress. Created properties representing the current status of the upload. Bound said properties to the template. Changed the properties in the appropriate functions.

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-upload',
@@ -9,8 +15,31 @@ export class UploadComponent implements OnInit {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait! Your clip is being uploaded.'
+  inSubmission = false;
+  percentage = 0;
+  showPercentage = false;
+  user: firebase.User | null = null;
 
-  constructor() { }
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+    ],
+    nonNullable: true
+  })
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth
+  ) {
+    auth.user.subscribe(user => this.user = user)
+  }
 
   ngOnInit(): void {
   }
@@ -21,10 +50,59 @@ export class UploadComponent implements OnInit {
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
 
     if (!this.file || this.file.type !== 'video/mp4') {
-      return
+      return;
     }
 
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    )
     this.nextStep = true;
+  }
+
+  uploadFile() {
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! Your clip is being uploaded.';
+    this.inSubmission = true;
+    this.showPercentage = true;
+
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+
+    const task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
+
+    task.percentageChanges().subscribe(progress => {
+      this.percentage = progress as number / 100;
+    })
+
+    task.snapshotChanges().pipe(
+      last(),
+      switchMap(() => clipRef.getDownloadURL())
+    ).subscribe({
+      next: (url) => {
+        const clip = {
+          uid: this.user?.uid,
+          displayName: this.user?.displayName,
+          title: this.title.value,
+          fileName: `${clipFileName}.mp4`,
+          url
+        }
+
+        console.log(clip);
+
+        this.alertColor = 'green';
+        this.alertMsg = 'Success! Your clip is now ready to share with the world!';
+        this.showPercentage = false;
+      },
+      error: (error) => {
+        this.alertColor = 'red';
+        this.alertMsg = 'Upload failed. Please ensure the file size is smaller than 25MB and try again later.';
+        this.inSubmission = true;
+        this.showPercentage = false;
+        console.log(error);
+      }
+    })
   }
 
 }
